d87fdcc89761109a3a1cbb9f6c828a15e6832bcd
sadaf-bahrami <sfbahramifar@gmail.com>
2025-10-20 18:29:51 +0330

    Add files via upload

---
+++ MyFirstApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("دمای سانتی‌گراد را وارد کنید: ");
+        double celsius = Convert.ToDouble(Console.ReadLine());
+
+        double fahrenheit = (celsius * 9 / 5) + 32;
+
+        Console.WriteLine($"{celsius} درجه سانتی‌گراد برابر است با {fahrenheit} درجه فارنهایت.");
+    }
+}
+
