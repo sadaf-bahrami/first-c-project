d6bf946e2065a0b0308d535a98f6134c20782545
sadaf-bahrami <sfbahramifar@gmail.com>
2025-11-02 22:26:42 +0330

    Add files via upload

---
+++ rishe/rishe2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+internal class rishe
+{
+    static void Main(string[] args)
+    {
+        Console.Write("enter your first number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("enter your second number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.Write("enter your third number: ");
+        int c = Convert.ToInt32(Console.ReadLine());
+        if(b*b-4*a*c>=0)
+        {
+            Console.WriteLine($"it has rishe");
+        }
+        else
+        {
+            Console.WriteLine($"it hasn't a rishe");
+        }
+    }
+    
+}
