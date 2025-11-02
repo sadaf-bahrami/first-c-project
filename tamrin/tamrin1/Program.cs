47be727a21a8444989c8e1213e0ae460ee9559b1
sadaf-bahrami <sfbahramifar@gmail.com>
2025-11-02 22:19:31 +0330

    Add files via upload

---
+++ tamrin/tamrin1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+internal class tamrin
+{
+    static void Main(string[] args)
+    {
+        Console.Write("enter your number:");
+        double inch = Convert.ToInt32(Console.ReadLine());
+        double cm = inch * 2.54;
+        Console.WriteLine($"{inch} inch & {cm} cm.");
+        Console.ReadKey();
+    }
+}
