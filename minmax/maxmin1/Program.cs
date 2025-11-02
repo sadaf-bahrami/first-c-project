8eb8cdb7a876816f05c2f069debfbcf29e351397
sadaf-bahrami <sfbahramifar@gmail.com>
2025-11-02 22:21:24 +0330

    Add files via upload

---
+++ minmax/maxmin1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+internal class maxmin
+{
+    static void Main(string[] args)
+    {
+        Console.Write("enter your first number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+         Console.Write("enter your second number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+         Console.Write("enter your third number: ");
+        int c = Convert.ToInt32(Console.ReadLine());
+        int max, min;
+        if(a>=b && a>=c)
+        {
+            max = a;
+        }
+        else if(b>=a && b>=c)
+        {
+            max = b;
+        }
+        else
+        {
+            max = c;
+        }
+        if(a<=b && a<=c)
+        {
+            min = a;
+        }
+        else if(b<=a && b<=c)
+        {
+            min = b;
+        }
+        else
+        {
+            min = c;
+        }
+        Console.WriteLine($"max is {max}");
+        Console.WriteLine($"min is {min}");
+        Console.ReadKey();
+    }
+}
