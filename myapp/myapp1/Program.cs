954d2b11d11078a0bb775a34a6b7c22c00d2a228
sadaf-bahrami <sfbahramifar@gmail.com>
2025-11-02 22:17:41 +0330

    Add files via upload

---
+++ myapp/myapp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+internal class myapp
+{
+    static void Main(string[] args)
+    {
+        Console.Write(" enter your number: ");
+        int a = Convert.ToInt32 (Console.ReadLine());
+        int mohit = 4 * a;
+        int masahat = a * a;
+        Console.WriteLine($"mohit is {mohit} and masahat is {masahat}.");
+        Console.ReadKey();
+    }
+    
+}
