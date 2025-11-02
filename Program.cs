1b2bcaf7aec2d214e11474eed6bc27feb65444ab
sadaf-bahrami <sfbahramifar@gmail.com>
2025-11-02 22:10:01 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write( "enter your name:");
+        string name = Console.ReadLine();
+        Console.Write("enter your family:");
+        string family = Console.ReadLine();
+        Console.WriteLine($"my name is {name} & my family is {family}.");
+        Console.ReadKey();
+
+    }
+}
