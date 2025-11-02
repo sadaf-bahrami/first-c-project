791aa833660094324194cfd6ca4d34c731dc4dc4
sadaf-bahrami <sfbahramifar@gmail.com>
2025-11-02 22:24:09 +0330

    Add files via upload

---
+++ evennumber/evennumber1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+internal class Evennumber
+{
+    static void Main(string[] args)
+    {
+        Console.Write("enter your number: ");
+        int a = Convert.ToInt32 (Console.ReadLine());
+        if(a % 2 == 0)
+        {
+            Console.WriteLine($"{a} is even number");
+        }
+        else
+        {
+            Console.WriteLine($"{a} is odd number");   
+        }
+        Console.ReadKey();
+    }
+}
+
