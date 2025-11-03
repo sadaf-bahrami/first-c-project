5fbc2a808517755bb5de4c6c05447f6959f71287
sadaf-bahrami <sfbahramifar@gmail.com>
2025-11-03 13:52:07 +0330

    Add files via upload

---
+++ mazrab7/mazrab71/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+internal class mazrab7
+{
+    static void Main(string[] args)
+    {
+        Console.Write("enter your number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        int result;
+        if (a%7==0)
+        {
+            Console.WriteLine("YES!");
+        }
+        else
+        {
+            result = a*3;
+            Console.WriteLine($"{result}");
+        }
+    }
+}
