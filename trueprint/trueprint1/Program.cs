fcf5a593320d62b7ead04819a6f5b8a7586e2902
sadaf-bahrami <sfbahramifar@gmail.com>
2025-11-03 13:46:00 +0330

    Add files via upload

---
+++ trueprint/trueprint1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+internal class trueprint
+{
+    static void Main(string[] args)
+    {
+        Console.Write("enter your number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        if(a < 10 && a % 2 == 0)
+        {
+            Console.WriteLine("TRUE");
+        }
+        else
+        {
+            Console.WriteLine("FALSE");           
+        }
+    }
+}
