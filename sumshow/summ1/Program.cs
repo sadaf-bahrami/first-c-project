d3c4e24a34c61651953fc1a145b3a7ae56318837
sadaf-bahrami <sfbahramifar@gmail.com>
2025-11-16 21:09:51 +0330

    Add files via upload

---
+++ sumshow/summ1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+internal class sumshow
+{
+    static void Main(string[]args)
+    {
+        /*do
+            {
+                Console.Write("Enter a number: ");
+                a = Int32.Parse(Console.ReadLine());
+                sum += a;
+            } while (a != 0);*/   
+        Console.Write("enter number: ");
+        int a = Int32.Parse(Console.ReadLine());
+        int sum = 0;
+        while (a != 0)
+        {
+            sum += a;
+            Console.Write("enter number: ");
+            a = Int32.Parse(Console.ReadLine());
+        }
+        Console.WriteLine($"sum = " + sum);
+        Console.ReadKey();
+    }
+}
