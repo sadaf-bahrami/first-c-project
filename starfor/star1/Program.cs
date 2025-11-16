d0cb887f61628c36922d998c9c06c87c9857ee77
sadaf-bahrami <sfbahramifar@gmail.com>
2025-11-16 20:25:38 +0330

    Add files via upload

---
+++ starfor/star1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+internal class starfor
+{
+    static void Main(string[]args)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("");
+        }
+         /*for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Console.WriteLine("*");
+            }
+            Console.WriteLine("");
+        }*/
+        Console.ReadKey();
+    }
+}
