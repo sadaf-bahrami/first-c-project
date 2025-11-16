0e66b68f67027e6794c7ab93337ccec71e55e273
sadaf-bahrami <sfbahramifar@gmail.com>
2025-11-16 20:24:01 +0330

    Add files via upload

---
+++ evennumber/evenum1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+internal class evennumber
+{
+    static void Main(string[]args)
+    {
+        int limit = 99;
+        for (int i = 10; i <= limit; i += 2)
+        {
+            Console.WriteLine($"{i} is even number");
+        }
+
+            /*for(int i = 10; i < 100; i++)
+            {
+                if (i % 2 == 0)
+                Console.WriteLine(i);
+            }*/
+
+            /*for (int i = 10; i < 100; i += 2)
+            {
+                Console.WriteLine(i);
+            }*/
+        Console.ReadKey();
+    }
+}
