01bcd522713f4d89660ae80021d3cf5dea27bc4f
sadaf-bahrami <sfbahramifar@gmail.com>
2025-11-16 19:30:29 +0330

    Add files via upload

---
+++ colorback/colorback1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace SwitchBackground
+{
+    internal class BackgroundColor1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your number: ");
+            int a = Int32.Parse(Console.ReadLine());
+            switch (a)
+            {
+                case 1:
+                     Console.BackgroundColor=ConsoleColor.Red;
+                     Console.Clear();
+                     break;
+                 case 2:
+                     Console.BackgroundColor=ConsoleColor.Blue;
+                     Console.Clear();
+                     break;
+                 default: 
+                     Console.BackgroundColor=ConsoleColor.Yellow;
+                     Console.Clear();
+                     break;
+            }
+        }
+    }
+}
