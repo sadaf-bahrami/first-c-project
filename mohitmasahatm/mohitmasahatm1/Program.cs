29c81691e59ea093bef3e6399eb923e59b2a25c2
sadaf-bahrami <sfbahramifar@gmail.com>
2025-11-21 23:57:01 +0330

    Add files via upload

---
+++ mohitmasahatm/mohitmasahatm1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+internal class mohitmasahatm
+{
+    static double Perimeter(double a, double b, double c)
+    {
+        return a + b + c;
+    }
+    static double Area(double a, double b, double c)
+    {
+        double s = (a + b + c) / 2; 
+        return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+    static void Main()
+    {
+        double a, b, c;
+
+        Console.Write("Enter side a: ");
+        a = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter side b: ");
+        b = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter side c: ");
+        c = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Perimeter = " + Perimeter(a, b, c));
+        Console.WriteLine("Area = " + Area(a, b, c));
+    }
+}
