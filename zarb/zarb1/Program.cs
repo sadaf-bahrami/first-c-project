cd221be008323312c905eec1ca684ddecf73dfa8
sadaf-bahrami <sfbahramifar@gmail.com>
2025-11-21 23:33:20 +0330

    Add files via upload

---
+++ zarb/zarb1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+internal class zarb
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("enter your number: ");
+            int a = Int32.Parse(Console.ReadLine());
+            int result = 1;
+            while (a!=0)
+            {
+                result *= a;
+                Console.Write("enter your number: ");
+                a = Int32.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"result = " + result);
+            Console.ReadKey();
+        }
+    }
